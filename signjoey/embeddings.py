ba680d06fc5125ce3b6cb683c7c546961c36c197
Samane80 <samasadi192@gmail.com>
2026-07-04 12:36:45 +0330

    fix the code

--- signjoey/embeddings.py
+++ signjoey/embeddings.py
@@ -3,7 +3,7 @@ import torch
 
 from torch import nn, Tensor
 import torch.nn.functional as F
-# from signjoey.helpers import freeze_params
+from signjoey.helpers import freeze_params
 
 
 def get_activation(activation_type):
@@ -36,46 +36,85 @@ def get_activation(activation_type):
 
 
 class MaskedNorm(nn.Module):
+    """
+        Original Code from:
+        https://discuss.pytorch.org/t/batchnorm-for-different-sized-samples-in-batch/44251/8
+    """
+
     def __init__(self, norm_type, num_groups, num_features):
         super().__init__()
         self.norm_type = norm_type
-        if norm_type == "batch":
+        if self.norm_type == "batch":
             self.norm = nn.BatchNorm1d(num_features=num_features)
-        elif norm_type == "group":
+        elif self.norm_type == "group":
             self.norm = nn.GroupNorm(num_groups=num_groups, num_channels=num_features)
-        elif norm_type == "layer":
-            self.norm = nn.LayerNorm(num_features)
+        elif self.norm_type == "layer":
+            self.norm = nn.LayerNorm(normalized_shape=num_features)
         else:
             raise ValueError("Unsupported Normalization Layer")
+
         self.num_features = num_features
 
     def forward(self, x: Tensor, mask: Tensor):
         if self.training:
             reshaped = x.reshape([-1, self.num_features])
             reshaped_mask = mask.reshape([-1, 1]) > 0
-            selected = torch.masked_select(reshaped, reshaped_mask).reshape([-1, self.num_features])
-            normed = self.norm(selected)
-            scattered = reshaped.masked_scatter(reshaped_mask, normed)
+            selected = torch.masked_select(reshaped, reshaped_mask).reshape(
+                [-1, self.num_features]
+            )
+            batch_normed = self.norm(selected)
+            scattered = reshaped.masked_scatter(reshaped_mask, batch_normed)
             return scattered.reshape([x.shape[0], -1, self.num_features])
         else:
             reshaped = x.reshape([-1, self.num_features])
-            normed = self.norm(reshaped)
-            return normed.reshape([x.shape[0], -1, self.num_features])
+            batched_normed = self.norm(reshaped)
+            return batched_normed.reshape([x.shape[0], -1, self.num_features])
 
 
+# TODO (Cihan): Spatial and Word Embeddings are pretty much the same
+#       We might as well convert them into a single module class.
+#       Only difference is the lut vs linear layers.
 class Embeddings(nn.Module):
-    def __init__(self, embedding_dim=64, num_heads=8, scale=False, scale_factor=None,
-                 norm_type=None, activation_type=None, vocab_size=0, padding_idx=1, **kwargs):
+
+    """
+    Simple embeddings class
+    """
+
+    # pylint: disable=unused-argument
+    def __init__(
+        self,
+        embedding_dim: int = 64,
+        num_heads: int = 8,
+        scale: bool = False,
+        scale_factor: float = None,
+        norm_type: str = None,
+        activation_type: str = None,
+        vocab_size: int = 0,
+        padding_idx: int = 1,
+        freeze: bool = False,
+        **kwargs
+    ):
+        """
+        Create new embeddings for the vocabulary.
+        Use scaling for the Transformer.
+
+        :param embedding_dim:
+        :param scale:
+        :param vocab_size:
+        :param padding_idx:
+        :param freeze: freeze the embeddings during training
+        """
         super().__init__()
+
         self.embedding_dim = embedding_dim
         self.vocab_size = vocab_size
-
-        self.lut = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
-        nn.init.normal_(self.lut.weight, mean=0, std=embedding_dim ** -0.5)
+        self.lut = nn.Embedding(vocab_size, self.embedding_dim, padding_idx=padding_idx)
 
         self.norm_type = norm_type
         if self.norm_type:
-            self.norm = MaskedNorm(norm_type, num_heads, embedding_dim)
+            self.norm = MaskedNorm(
+                norm_type=norm_type, num_groups=num_heads, num_features=embedding_dim
+            )
 
         self.activation_type = activation_type
         if self.activation_type:
@@ -83,31 +122,84 @@ class Embeddings(nn.Module):
 
         self.scale = scale
         if self.scale:
-            self.scale_factor = scale_factor if scale_factor else math.sqrt(embedding_dim)
+            if scale_factor:
+                self.scale_factor = scale_factor
+            else:
+                self.scale_factor = math.sqrt(self.embedding_dim)
 
-    def forward(self, x: Tensor, mask: Tensor = None):
+        if freeze:
+            freeze_params(self)
+
+    # pylint: disable=arguments-differ
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
+        """
+        Perform lookup for input `x` in the embedding table.
+
+        :param mask: token masks
+        :param x: index in the vocabulary
+        :return: embedded representation for `x`
+        """
+
         x = self.lut(x)
-        if self.scale:
-            x = x * self.scale_factor
+
         if self.norm_type:
             x = self.norm(x, mask)
+
         if self.activation_type:
             x = self.activation(x)
-        return x
+
+        if self.scale:
+            return x * self.scale_factor
+        else:
+            return x
+
+    def __repr__(self):
+        return "%s(embedding_dim=%d, vocab_size=%d)" % (
+            self.__class__.__name__,
+            self.embedding_dim,
+            self.vocab_size,
+        )
 
 
 class SpatialEmbeddings(nn.Module):
-    def __init__(self, embedding_dim, input_size, num_heads, norm_type=None,
-                 activation_type=None, scale=False, scale_factor=None, **kwargs):
+
+    """
+    Simple Linear Projection Layer
+    (For encoder outputs to predict glosses)
+    """
+
+    # pylint: disable=unused-argument
+    def __init__(
+        self,
+        embedding_dim: int,
+        input_size: int,
+        num_heads: int,
+        freeze: bool = False,
+        norm_type: str = None,
+        activation_type: str = None,
+        scale: bool = False,
+        scale_factor: float = None,
+        **kwargs
+    ):
+        """
+        Create new embeddings for the vocabulary.
+        Use scaling for the Transformer.
+
+        :param embedding_dim:
+        :param input_size:
+        :param freeze: freeze the embeddings during training
+        """
         super().__init__()
+
         self.embedding_dim = embedding_dim
         self.input_size = input_size
-
-        self.ln = nn.Linear(input_size, embedding_dim)
+        self.ln = nn.Linear(self.input_size, self.embedding_dim)
 
         self.norm_type = norm_type
         if self.norm_type:
-            self.norm = MaskedNorm(norm_type, num_heads, embedding_dim)
+            self.norm = MaskedNorm(
+                norm_type=norm_type, num_groups=num_heads, num_features=embedding_dim
+            )
 
         self.activation_type = activation_type
         if self.activation_type:
@@ -115,14 +207,38 @@ class SpatialEmbeddings(nn.Module):
 
         self.scale = scale
         if self.scale:
-            self.scale_factor = scale_factor if scale_factor else math.sqrt(embedding_dim)
+            if scale_factor:
+                self.scale_factor = scale_factor
+            else:
+                self.scale_factor = math.sqrt(self.embedding_dim)
 
-    def forward(self, x: Tensor, mask: Tensor):
+        if freeze:
+            freeze_params(self)
+
+    # pylint: disable=arguments-differ
+    def forward(self, x: Tensor, mask: Tensor) -> Tensor:
+        """
+        :param mask: frame masks
+        :param x: input frame features
+        :return: embedded representation for `x`
+        """
+
         x = self.ln(x)
+
         if self.norm_type:
             x = self.norm(x, mask)
+
         if self.activation_type:
             x = self.activation(x)
+
         if self.scale:
-            x = x * self.scale_factor
-        return x
+            return x * self.scale_factor
+        else:
+            return x
+
+    def __repr__(self):
+        return "%s(embedding_dim=%d, input_size=%d)" % (
+            self.__class__.__name__,
+            self.embedding_dim,
+            self.input_size,
+        )
--- signjoey/transformer_layers.py
+++ signjoey/transformer_layers.py
@@ -1,152 +1,207 @@
 # -*- coding: utf-8 -*-
+
 import math
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import Tensor
 
 
+# pylint: disable=arguments-differ
 class MultiHeadedAttention(nn.Module):
     """
-    نسخه FP32 سازگار با کد اصلی signjoey/SLT
-    اسم attributeها عمداً مثل نسخه کوانتیزه نگه داشته شده.
+    Multi-Head Attention module from "Attention is All You Need"
+
+    Implementation modified from OpenNMT-py.
+    https://github.com/OpenNMT/OpenNMT-py
     """
 
-    def __init__(self, num_heads: int, size: int, dropout: float = 0.1,
-                 qkv_bias=False, focusing_factor: int = 2, kernel_size: int = 5,
-                 causal: bool = False, is_self_attention: bool = True):
-        super().__init__()
+    def __init__(self, num_heads: int, size: int, dropout: float = 0.1):
+        """
+        Create a multi-headed attention layer.
+        :param num_heads: the number of heads
+        :param size: model size (must be divisible by num_heads)
+        :param dropout: probability of dropping a unit
+        """
+        super(MultiHeadedAttention, self).__init__()
+
+        assert size % num_heads == 0
+
+        self.head_size = head_size = size // num_heads
+        self.model_size = size
         self.num_heads = num_heads
-        self.head_size = size // num_heads
-        self.dim = size
-        self.focusing_factor = focusing_factor
-        self.causal = causal
-        self.is_self_attention = is_self_attention
 
-        # اسم attributeها دقیقاً مثل نسخه Quant
-        self.q_layer = nn.Linear(size, size, bias=qkv_bias)
-        self.k_layer = nn.Linear(size, size, bias=qkv_bias)
-        self.v_layer = nn.Linear(size, size, bias=qkv_bias)
+        self.k_layer = nn.Linear(size, num_heads * head_size)
+        self.v_layer = nn.Linear(size, num_heads * head_size)
+        self.q_layer = nn.Linear(size, num_heads * head_size)
+
         self.output_layer = nn.Linear(size, size)
+        self.softmax = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
 
-        self.attn_drop = nn.Dropout(dropout)
-        self.proj_drop = nn.Dropout(dropout)
+    def forward(self, k: Tensor, v: Tensor, q: Tensor, mask: Tensor = None):
+        """
+        Computes multi-headed attention.
 
-        self.dwc = nn.Conv2d(
-            self.head_size, self.head_size, kernel_size=(1, kernel_size),
-            padding=(0, kernel_size // 2), groups=self.head_size, bias=True
+        :param k: keys   [B, M, D] with M being the sentence length.
+        :param v: values [B, M, D]
+        :param q: query  [B, M, D]
+        :param mask: optional mask [B, 1, M]
+        :return:
+        """
+        batch_size = k.size(0)
+        num_heads = self.num_heads
+
+        # project the queries (q), keys (k), and values (v)
+        k = self.k_layer(k)
+        v = self.v_layer(v)
+        q = self.q_layer(q)
+
+        # reshape q, k, v for our computation to [batch_size, num_heads, ..]
+        k = k.view(batch_size, -1, num_heads, self.head_size).transpose(1, 2)
+        v = v.view(batch_size, -1, num_heads, self.head_size).transpose(1, 2)
+        q = q.view(batch_size, -1, num_heads, self.head_size).transpose(1, 2)
+
+        # compute scores
+        q = q / math.sqrt(self.head_size)
+
+        # batch x num_heads x query_len x key_len
+        scores = torch.matmul(q, k.transpose(2, 3))
+
+        # apply the mask (if we have one)
+        # we add a dimension for the heads to it below: [B, 1, 1, M]
+        if mask is not None:
+            scores = scores.masked_fill(~mask.unsqueeze(1), float("-inf"))
+
+        # apply attention dropout and compute context vectors.
+        attention = self.softmax(scores)
+        attention = self.dropout(attention)
+
+        # get context vector (select values with attention) and reshape
+        # back to [B, M, D]
+        context = torch.matmul(attention, v)
+        context = (
+            context.transpose(1, 2)
+            .contiguous()
+            .view(batch_size, -1, num_heads * self.head_size)
         )
 
-    def _focused_map(self, x):
-        x = F.relu(x) + 1e-6
-        xp = x * x if self.focusing_factor == 2 else x * x * x
-        x_norm = torch.max(torch.abs(x), dim=-1, keepdim=True)[0] + 1e-8
-        xp_norm = torch.max(torch.abs(xp), dim=-1, keepdim=True)[0] + 1e-8
-        scale = x_norm / (xp_norm + 1e-6)
-        return scale * xp
-
-    def forward(self, query, key=None, value=None, mask=None):
-        if key is None:
-            key = query
-        if value is None:
-            value = query   # مهم برای سازگاری با فراخوانی قدیمی
-
-        B, Nq, C = query.shape
-        Nk = key.shape[1]
-
-        q = self.q_layer(query)
-        k = self.k_layer(key)
-        v = self.v_layer(value)
-
-        q = q.reshape(B, Nq, self.num_heads, self.head_size).transpose(1, 2)
-        k = k.reshape(B, Nk, self.num_heads, self.head_size).transpose(1, 2)
-        v = v.reshape(B, Nk, self.num_heads, self.head_size).transpose(1, 2)
-
-        q = self._focused_map(q)
-        k = self._focused_map(k)
-
-        # Key-padding mask
-        key_valid_mask = None
-        if mask is not None and not self.causal:
-            key_valid_mask = mask
-            if key_valid_mask.dim() == 3:
-                key_valid_mask = key_valid_mask.squeeze(1).unsqueeze(1).unsqueeze(-1)
-            key_valid_mask = key_valid_mask.to(k.dtype)
-            k = k * key_valid_mask
-            v = v * key_valid_mask
-
-        if self.causal:
-            kv = torch.einsum("bhnd,bhne->bhnde", k, v)
-            kv_cumsum = torch.cumsum(kv, dim=2)
-            context = torch.einsum("bhnd,bhnde->bhne", q, kv_cumsum)
-
-            k_cumsum = torch.cumsum(k, dim=2)
-            z = torch.einsum("bhnd,bhnd->bhn", q, k_cumsum).unsqueeze(-1)
-        else:
-            kv = torch.einsum("bhnd,bhne->bhde", k, v)
-            context = torch.einsum("bhnd,bhde->bhne", q, kv)
-
-            if key_valid_mask is not None:
-                valid_counts = key_valid_mask.sum(dim=2, keepdim=True).clamp(min=1)
-                k_mean = (k.sum(dim=-2, keepdim=True) / valid_counts).squeeze(-2)
-            else:
-                k_mean = k.mean(dim=-2)
-            z = torch.einsum("bhnd,bhd->bhn", q, k_mean).unsqueeze(-1)
-
-        z = 1.0 / (z + 1e-6)
-        context = context * z
-
-        if self.is_self_attention:
-            v_1d = v.permute(0, 1, 3, 2).reshape(
-                B * self.num_heads, self.head_size, 1, Nk
-            )
-            dwc_out = self.dwc(v_1d)
-            dwc_out = dwc_out.reshape(
-                B, self.num_heads, self.head_size, Nk
-            ).permute(0, 1, 3, 2)
-            context = context + dwc_out
-
-        context = context.transpose(1, 2).contiguous().view(B, Nq, C)
-        context = self.proj_drop(context)
         output = self.output_layer(context)
+
         return output
 
 
+# pylint: disable=arguments-differ
 class PositionwiseFeedForward(nn.Module):
-    def __init__(self, input_size: int, ff_size: int, dropout: float = 0.1):
-        super().__init__()
-        self.pwff_layer = nn.ModuleList([
-            nn.Linear(input_size, ff_size),   # [0]
-            nn.GELU(),                        # [1]
-            nn.Dropout(dropout),              # [2]
-            nn.Linear(ff_size, input_size),   # [3]
-            nn.Dropout(dropout),              # [4]
-        ])
+    """
+    Position-wise Feed-forward layer
+    Projects to ff_size and then back down to input_size.
+    """
+
+    def __init__(self, input_size, ff_size, dropout=0.1):
+        """
+        Initializes position-wise feed-forward layer.
+        :param input_size: dimensionality of the input.
+        :param ff_size: dimensionality of intermediate representation
+        :param dropout:
+        """
+        super(PositionwiseFeedForward, self).__init__()
+        self.layer_norm = nn.LayerNorm(input_size, eps=1e-6)
+        self.pwff_layer = nn.Sequential(
+            nn.Linear(input_size, ff_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(ff_size, input_size),
+            nn.Dropout(dropout),
+        )
 
     def forward(self, x):
-        x = self.pwff_layer[0](x)
-        x = self.pwff_layer[1](x)
-        x = self.pwff_layer[2](x)
-        x = self.pwff_layer[3](x)
-        x = self.pwff_layer[4](x)
-        return x
+        x_norm = self.layer_norm(x)
+        return self.pwff_layer(x_norm) + x
+
+
+# pylint: disable=arguments-differ
+class PositionalEncoding(nn.Module):
+    """
+    Pre-compute position encodings (PE).
+    In forward pass, this adds the position-encodings to the
+    input for as many time steps as necessary.
+
+    Implementation based on OpenNMT-py.
+    https://github.com/OpenNMT/OpenNMT-py
+    """
+
+    def __init__(self, size: int = 0, max_len: int = 5000):
+        """
+        Positional Encoding with maximum length max_len
+        :param size:
+        :param max_len:
+        :param dropout:
+        """
+        if size % 2 != 0:
+            raise ValueError(
+                "Cannot use sin/cos positional encoding with "
+                "odd dim (got dim={:d})".format(size)
+            )
+        pe = torch.zeros(max_len, size)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            (torch.arange(0, size, 2, dtype=torch.float) * -(math.log(10000.0) / size))
+        )
+        pe[:, 0::2] = torch.sin(position.float() * div_term)
+        pe[:, 1::2] = torch.cos(position.float() * div_term)
+        pe = pe.unsqueeze(0)  # shape: [1, size, max_len]
+        super(PositionalEncoding, self).__init__()
+        self.register_buffer("pe", pe)
+        self.dim = size
+
+    def forward(self, emb):
+        """Embed inputs.
+        Args:
+            emb (FloatTensor): Sequence of word vectors
+                ``(seq_len, batch_size, self.dim)``
+        """
+        # Add position encodings
+        return emb + self.pe[:, : emb.size(1)]
 
 
 class TransformerEncoderLayer(nn.Module):
-    def __init__(self, size: int = 0, ff_size: int = 0, num_heads: int = 0, dropout: float = 0.1):
-        super().__init__()
-        self.size = size
+    """
+    One Transformer encoder layer has a Multi-head attention layer plus
+    a position-wise feed-forward layer.
+    """
+
+    def __init__(
+        self, size: int = 0, ff_size: int = 0, num_heads: int = 0, dropout: float = 0.1
+    ):
+        """
+        A single Transformer layer.
+        :param size:
+        :param ff_size:
+        :param num_heads:
+        :param dropout:
+        """
+        super(TransformerEncoderLayer, self).__init__()
+
         self.layer_norm = nn.LayerNorm(size, eps=1e-6)
-        self.src_src_att = MultiHeadedAttention(
-            num_heads=num_heads, size=size, dropout=dropout,
-            focusing_factor=2, kernel_size=5, is_self_attention=True
-        )
+        self.src_src_att = MultiHeadedAttention(num_heads, size, dropout=dropout)
         self.feed_forward = PositionwiseFeedForward(
             input_size=size, ff_size=ff_size, dropout=dropout
         )
         self.dropout = nn.Dropout(dropout)
+        self.size = size
 
+    # pylint: disable=arguments-differ
     def forward(self, x: Tensor, mask: Tensor) -> Tensor:
+        """
+        Forward pass for a single transformer encoder layer.
+        First applies layer norm, then self attention,
+        then dropout with residual connection (adding the input to the result),
+        and then a position-wise feed-forward layer.
+
+        :param x: layer input
+        :param mask: input mask
+        :return: output tensor
+        """
         x_norm = self.layer_norm(x)
         h = self.src_src_att(x_norm, x_norm, x_norm, mask)
         h = self.dropout(h) + x
@@ -155,51 +210,67 @@ class TransformerEncoderLayer(nn.Module):
 
 
 class TransformerDecoderLayer(nn.Module):
-    def __init__(self, size: int = 0, ff_size: int = 0, num_heads: int = 0, dropout: float = 0.1):
-        super().__init__()
+    """
+    Transformer decoder layer.
+
+    Consists of self-attention, source-attention, and feed-forward.
+    """
+
+    def __init__(
+        self, size: int = 0, ff_size: int = 0, num_heads: int = 0, dropout: float = 0.1
+    ):
+        """
+        Represents a single Transformer decoder layer.
+
+        It attends to the source representation and the previous decoder states.
+
+        :param size: model dimensionality
+        :param ff_size: size of the feed-forward intermediate layer
+        :param num_heads: number of heads
+        :param dropout: dropout to apply to input
+        """
+        super(TransformerDecoderLayer, self).__init__()
         self.size = size
-        self.trg_trg_att = MultiHeadedAttention(
-            num_heads=num_heads, size=size, dropout=dropout,
-            focusing_factor=2, causal=True, is_self_attention=True
-        )
-        self.src_trg_att = MultiHeadedAttention(
-            num_heads=num_heads, size=size, dropout=dropout,
-            focusing_factor=2, is_self_attention=False
-        )
+
+        self.trg_trg_att = MultiHeadedAttention(num_heads, size, dropout=dropout)
+        self.src_trg_att = MultiHeadedAttention(num_heads, size, dropout=dropout)
+
         self.feed_forward = PositionwiseFeedForward(
             input_size=size, ff_size=ff_size, dropout=dropout
         )
+
         self.x_layer_norm = nn.LayerNorm(size, eps=1e-6)
         self.dec_layer_norm = nn.LayerNorm(size, eps=1e-6)
+
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x: Tensor, memory: Tensor, src_mask: Tensor = None, trg_mask: Tensor = None):
-        # decoder self-attention
+    # pylint: disable=arguments-differ
+    def forward(
+        self,
+        x: Tensor = None,
+        memory: Tensor = None,
+        src_mask: Tensor = None,
+        trg_mask: Tensor = None,
+    ) -> Tensor:
+        """
+        Forward pass of a single Transformer decoder layer.
+
+        :param x: inputs
+        :param memory: source representations
+        :param src_mask: source mask
+        :param trg_mask: target mask (so as to not condition on future steps)
+        :return: output tensor
+        """
+        # decoder/target self-attention
         x_norm = self.x_layer_norm(x)
         h1 = self.trg_trg_att(x_norm, x_norm, x_norm, mask=trg_mask)
         h1 = self.dropout(h1) + x
 
-        # source attention
+        # source-target attention
         h1_norm = self.dec_layer_norm(h1)
-        h2 = self.src_trg_att(h1_norm, memory, memory, mask=src_mask)
+        h2 = self.src_trg_att(memory, memory, h1_norm, mask=src_mask)
 
-        # feed-forward
+        # final position-wise feed-forward layer
         o = self.feed_forward(self.dropout(h2) + h1)
+
         return o
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, size: int, max_len: int = 5000):
-        super().__init__()
-        pe = torch.zeros(max_len, size)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, size, 2, dtype=torch.float) * -(math.log(10000.0) / size)
-        )
-        pe[:, 0::2] = torch.sin(position.float() * div_term)
-        pe[:, 1::2] = torch.cos(position.float() * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
-
-    def forward(self, emb):
-        return emb + self.pe[:, :emb.size(1)]
